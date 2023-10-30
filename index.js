f731eaa0f973c0de4aef89ad334b0160f06aade2
xio9971 <xio9971@daum.net>
2023-10-30 10:20:31 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+const config = require('./config/key');
+
+//application/x-www-form-unlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/jspn
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://xio9971:wmfdldi5017@clusternode.zkwisj5.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
                 .then(() => console.log('MongoDb Connected..'))
                 .catch(err => console.log(err))
 
@@ -11,8 +19,24 @@ mongoose.connect('mongodb+srv://xio9971:wmfdldi5017@clusternode.zkwisj5.mongodb.
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!ㅇㅇ ㅎㅇㅎㅇ')
+    res.send('Hello World!ㅇㅇ ㅎㅇㅎㅇsss')
 })
+
+app.post('/register', async (req, res) => {
+    
+    const user = new User(req.body) 
+    await user.save().then(() => {
+        res.status(200).json({
+            success: true
+        })
+    })
+    .catch((err) => {
+        if(err) return res.json({success : false, err})
+    })
+
+
+})
+
   
 app.listen(port, () => {
 console.log(`Example app listening on port ${port}`)
